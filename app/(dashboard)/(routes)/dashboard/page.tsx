7bd6497a10cc8a07faf7c6d8376ac783e47b527e
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-08-05 20:27:03 +0530

    initialized next.js app and implmented authentication

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p>Dashboard Page (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      LandingPage (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
