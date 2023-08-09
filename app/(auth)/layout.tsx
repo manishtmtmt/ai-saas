43467f12faca8a4f6c3d9a6de8fc699446abcd81
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-08-09 17:53:25 +0530

    auth provider

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center justify-center h-full">{children}</div>
+    <div className="h-full flex items-center justify-center pt-8">
+      {children}
+    </div>
   );
 };
 
